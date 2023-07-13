c19e599ceaec93636c6d07f585b78b4c02a2b924
KarolisVenckus <karolis.venckus12@gmail.com>
2023-07-13 08:40:10 +0300

    grenade animations and timers added

--- shooter.py
+++ shooter.py
@@ -233,19 +233,41 @@ class Grenade(pygame.sprite.Sprite):
           self.rect.x += dx
           self.rect.y += dy
 
+          #countdown timer
+          self.timer -= 1
+          if self.timer <= 0:
+               self.kill()
+               explosion = Explosion(self.rect.x, self.rect.y, 0.5)
+               explosion_group.add(explosion)
+
 class Explosion(pygame.sprite.Sprite):
      def __init__(self, x, y, scale):
           pygame.sprite.Sprite.__init__(self)
           self.images = []
           for num in range(1, 6):
                img = pygame.image.load(f'img/explosion/exp{num}.png').convert_alpha()
-               img = pygame.image.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+               img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
                self.images.append(img)
           self.frame_index = 0
           self.image = self.images[self.frame_index]
           self.rect = self.image.get_rect()
           self.rect.center = (x, y)
           self.counter = 0
+
+     
+     def update(self):
+          EXPLOSION_SPEED = 4
+          #update explosion animation
+          self.counter += 1
+
+          if self.counter >= EXPLOSION_SPEED:
+               self.counter = 0
+               self.frame_index += 1
+               #if the animation is complete then delete the explosion
+               if self.frame_index >= len(self.images):
+                    self.kill()
+               else:
+                    self.image = self.images[self.frame_index]
 
 
 
@@ -277,8 +299,10 @@ while run:
     #update and draw groups
     bullet_group.update()
     grenade_group.update()
+    explosion_group.update()
     bullet_group.draw(screen)
     grenade_group.draw(screen)
+    explosion_group.draw(screen)
 
     #update player actions
     if player.alive:
